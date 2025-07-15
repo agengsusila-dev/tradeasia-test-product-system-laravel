0676daa761f84b7e0c0da23de7b7818b778a9e8a
gh-ageng-susila <agengsusila@gmail.com>
2025-07-15 23:04:56 +0700

    feat(api): create public api
    completing task 2 in technical test from tradeasia

---
+++ app/Http/Controllers/Api/ProductApiController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+
+class ProductApiController extends Controller
+{
+    public function index(Request $request, $lang)
+    {
+        if (!in_array($lang, ['en', 'id'])) {
+            return response()->json(['error' => 'Invalid lang'], 400);
+        }
+
+        $products = Product::paginate(10);
+
+        $data = $products->map(function ($product) use ($lang) {
+            return [
+                'id' => Crypt::encryptString($product->id),
+                'name' => $product->name[$lang] ?? '-',
+                'hs_code' => $product->hs_code,
+                'cas_number' => $product->cas_number,
+                'image_url' => $product->image ? asset('storage/' . $product->image) : null,
+                'description' => $product->description[$lang] ?? '-',
+                'application' => $product->application[$lang] ?? '-',
+                'meta' => [
+                    'meta_title' => $product->meta_title[$lang] ?? '',
+                    'meta_keyword' => $product->meta_keyword[$lang] ?? '',
+                    'meta_description' => $product->meta_description[$lang] ?? '',
+                ],
+            ];
+        });
+
+        return response()->json($data);
+    }
+
+
+    public function show($lang, $id)
+    {
+        if (!in_array($lang, ['en', 'id'])) {
+            return response()->json(['error' => 'Invalid lang'], 400);
+        }
+
+        $product = Product::find(Crypt::decryptString($id));
+
+        if (!$product) {
+            return response()->json(['error' => 'Produk tidak ditemukan'], 404);
+        }
+
+        return response()->json([
+            'name' => $product->name[$lang] ?? '-',
+            'hs_code' => $product->hs_code,
+            'cas_number' => $product->cas_number,
+            'image_url' => $product->image ? asset('storage/' . $product->image) : null,
+            'description' => $product->description[$lang] ?? '-',
+            'application' => $product->application[$lang] ?? '-',
+            'meta' => [
+                'meta_title' => $product->meta_title[$lang] ?? '',
+                'meta_keyword' => $product->meta_keyword[$lang] ?? '',
+                'meta_description' => $product->meta_description[$lang] ?? '',
+            ],
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProductApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('api')->group(function () {
+    Route::prefix('{lang}')->middleware(['throttle:60,1'])->group(function () {
+        Route::get('/products', [ProductApiController::class, 'index']);
+        Route::get('/products/{id}', [ProductApiController::class, 'show']);
+    });
 });
